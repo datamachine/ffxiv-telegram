8ea108793d94e9bd006a5f28e974ba06b9ac2071
Vincent Castellano <vincentcastellano@gmail.com>
2026-03-30 00:48:07 -0700

    chore: point manifest at public repository

--- tests/FFXIVTelegram.Tests/Bootstrap/PluginManifestTests.cs
+++ tests/FFXIVTelegram.Tests/Bootstrap/PluginManifestTests.cs
@@ -6,12 +6,22 @@ using Xunit;
 public sealed class PluginManifestTests
 {
     [Fact]
-    public void ManifestIncludesNonEmptyTagsArray()
+    public void ManifestIncludesRequiredPublicDistributionFields()
     {
         var manifestPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../src/FFXIVTelegram/FFXIVTelegram.json"));
         using var document = JsonDocument.Parse(File.ReadAllText(manifestPath));
 
-        Assert.True(document.RootElement.TryGetProperty("Tags", out var tagsElement));
+        Assert.Equal("vcastellano", document.RootElement.GetProperty("Author").GetString());
+        Assert.Equal("FFXIV Telegram", document.RootElement.GetProperty("Name").GetString());
+        Assert.Equal("Bridges FFXIV chat and Telegram.", document.RootElement.GetProperty("Description").GetString());
+        Assert.Equal("Bridging Eorzea and Telegram.", document.RootElement.GetProperty("Punchline").GetString());
+        Assert.Equal("FFXIVTelegram", document.RootElement.GetProperty("InternalName").GetString());
+        Assert.Equal("any", document.RootElement.GetProperty("ApplicableVersion").GetString());
+        Assert.Equal(14, document.RootElement.GetProperty("DalamudApiLevel").GetInt32());
+        Assert.Equal(0, document.RootElement.GetProperty("LoadPriority").GetInt32());
+        Assert.Equal("https://github.com/datamachine/ffxiv-telegram", document.RootElement.GetProperty("RepoUrl").GetString());
+
+        var tagsElement = document.RootElement.GetProperty("Tags");
         Assert.Equal(JsonValueKind.Array, tagsElement.ValueKind);
         Assert.NotEmpty(tagsElement.EnumerateArray());
     }
